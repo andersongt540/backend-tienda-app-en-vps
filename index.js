ec5b7caa6503a9397ad45bd5b80678f1b86ebad5
andersongt540 <56943553+andersongt540@users.noreply.github.com>
2026-08-06 13:21:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { initDB } = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const storeRoutes = require('./routes/storeRoutes');
+const { initDB } = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
+const storeRoutes = require('./src/routes/storeRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
