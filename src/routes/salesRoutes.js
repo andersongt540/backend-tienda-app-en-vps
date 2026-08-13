a1d44eafc5fd864c29c21d0b0b7aff098ccd5a6e
andersongt540 <56943553+andersongt540@users.noreply.github.com>
2026-08-12 21:52:53 -0400

    .

--- src/routes/salesRoutes.js
+++ src/routes/salesRoutes.js
@@ -1,10 +1,8 @@
-// src/routes/salesRoutes.js
 const express = require('express');
 const router = express.Router();
 const salesController = require('../controllers/salesController');
 
 router.post('/register', salesController.registerSale);
-router.get('/recent', salesController.getRecentSales);
-router.delete('/:id', salesController.deleteSale); // <-- Nueva ruta
+router.delete('/:id', salesController.deleteSale);
 
 module.exports = router;
